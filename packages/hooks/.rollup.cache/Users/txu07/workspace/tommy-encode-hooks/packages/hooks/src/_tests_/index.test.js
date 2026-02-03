446cdc9e4ce49a290e0d2c3bf0fe8b2cb0973a3e
Tommy Xu （徐锦涛） <Tommy.Xu@merkle.com>
2026-02-03 15:11:37 +0800

    chore: ignore rollup cache

--- packages/hooks/.rollup.cache/Users/txu07/workspace/tommy-encode-hooks/packages/hooks/src/_tests_/index.test.js
+++
@@ -1,9 +0,0 @@
-import * as encodeHooks from '..';
-describe('encodeHooks', function () {
-    test('exports modules should be defined', function () {
-        Object.keys(encodeHooks).forEach(function (module) {
-            expect(encodeHooks[module]).toBeDefined();
-        });
-    });
-});
-//# sourceMappingURL=index.test.js.map
--- packages/hooks/.rollup.cache/Users/txu07/workspace/tommy-encode-hooks/packages/hooks/src/useToggle/demo/basic.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * title: 基础用法
- * desc: 默认为 boolean 切换，基础用法与 useBoolean 一致。
- */
-import React from 'react';
-declare const _default: () => React.JSX.Element;
-export default _default;
